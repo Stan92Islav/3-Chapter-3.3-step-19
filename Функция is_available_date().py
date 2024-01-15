0f29fbd871cabba880467b73bfd729e810680187
Stan92Islav <156432105+Stan92Islav@users.noreply.github.com>
2024-01-15 10:36:38 +0300

    Add files via upload

---
+++ Функция is_available_date().py
@@ -0,0 +1,133 @@
+'''Во время визита очередного гостя сотрудникам отеля приходится проверять, доступна ли та или иная дата для бронирования номера.
+
+Реализуйте функцию is_available_date(), которая принимает два аргумента в следующем порядке:
+
+booked_dates — список строковых дат, недоступных для бронирования. Элементом списка является либо одиночная дата, либо
+период (две даты через дефис). Например:
+['04.11.2021', '05.11.2021-09.11.2021']
+date_for_booking — одиночная строковая дата или период (две даты через дефис), на которую гость желает забронировать
+номер. Например:
+'01.11.2021' или '01.11.2021-04.11.2021'
+Функция is_available_date() должна возвращать True, если дата или период date_for_booking полностью доступна для
+бронирования. В противном случае функция должна возвращать False.
+
+Примечание 1. Гарантируется, что в периоде левая дата всегда меньше правой.
+
+Примечание 2. В периоде (две даты через дефис) граничные даты включены.
+
+Примечание 3. В тестирующую систему сдайте программу, содержащую только необходимую функцию is_available_date(), но не
+код, вызывающий ее.'''
+
+from datetime import datetime
+def sd(s):
+    return datetime.strptime(s, '%d.%m.%Y')
+def dk(spd):
+    return (sd(spd[:10]), sd(spd[11:])) if '-' in spd else (sd(spd), sd(spd))
+def is_available_date(sp, d):
+    for x in sp:
+        if not(dk(x)[1] < dk(d)[0] or dk(x)[0] > dk(d)[1]):
+            return False
+    return True
+
+
+# INPUT DATA:
+
+# TEST_1:
+dates = ['04.11.2021', '05.11.2021-09.11.2021']
+some_date = '01.11.2021'
+print(is_available_date(dates, some_date))
+
+# TEST_2:
+dates = ['04.11.2021', '05.11.2021-09.11.2021']
+some_date = '01.11.2021-04.11.2021'
+print(is_available_date(dates, some_date))
+
+# TEST_3:
+dates = ['04.11.2021', '05.11.2021-09.11.2021']
+some_date = '06.11.2021'
+print(is_available_date(dates, some_date))
+
+# TEST_4:
+dates = ['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021']
+some_date = '12.11.2021'
+print(is_available_date(dates, some_date))
+
+# TEST_5:
+dates = ['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021']
+some_date = '09.11.2021'
+print(is_available_date(dates, some_date))
+
+# TEST_6:
+dates = ['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021']
+some_date = '15.11.2021'
+print(is_available_date(dates, some_date))
+
+# TEST_7:
+dates = ['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021']
+some_date = '17.11.2021'
+print(is_available_date(dates, some_date))
+
+# TEST_8:
+dates = ['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021']
+some_date = '22.11.2021-25.11.2021'
+print(is_available_date(dates, some_date))
+
+# TEST_9:
+dates = ['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021']
+some_date = '01.11.2021-04.11.2021'
+print(is_available_date(dates, some_date))
+
+# TEST_10:
+dates = ['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021']
+some_date = '02.11.2021-05.11.2021'
+print(is_available_date(dates, some_date))
+
+# TEST_11:
+dates = ['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021']
+some_date = '10.11.2021-14.11.2021'
+print(is_available_date(dates, some_date))
+
+# TEST_12:
+dates = ['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021']
+some_date = '03.11.2021-05.11.2021'
+print(is_available_date(dates, some_date))
+
+# TEST_13:
+dates = ['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021']
+some_date = '09.11.2021-10.11.2021'
+print(is_available_date(dates, some_date))
+
+# TEST_14:
+dates = ['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021']
+some_date = '06.11.2021-08.11.2021'
+print(is_available_date(dates, some_date))
+
+# TEST_15:
+dates = ['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021']
+some_date = '14.11.2021-22.11.2021'
+print(is_available_date(dates, some_date))
+
+# TEST_16:
+dates = ['14.09.2022-14.10.2022']
+some_date = '20.09.2022'
+print(is_available_date(dates, some_date))
+
+# TEST_17:
+dates = ['14.09.2022-14.10.2022']
+some_date = '14.11.2022'
+print(is_available_date(dates, some_date))
+
+# TEST_18:
+dates = ['14.09.2022-14.10.2022']
+some_date = '15.11.2022-16.11.2022'
+print(is_available_date(dates, some_date))
+
+# TEST_19:
+dates = ['14.09.2022-14.10.2022']
+some_date = '14.09.2022-22.09.2022'
+print(is_available_date(dates, some_date))
+
+# TEST_20:
+dates = ['02.11.2021', '05.11.2021-09.11.2021']
+some_date = '04.11.2021'
+print(is_available_date(dates, some_date))
